f6739817d9baa233181ce379b9764e50a9b68e97
PremChand193 <116290642+PremChand193@users.noreply.github.com>
2023-02-22 20:02:25 +0530

    Add files via upload

---
+++ coding.js
@@ -0,0 +1,31 @@
+let display=document.getElementById('display');
+
+let buttons=Array.from(document.getElementsByClassName('button'));
+buttons.map(button => {
+  button.addEventListener('click', (e)=> {
+    switch (e.target.innerText) {
+      case 'C':
+        display.innerText=' ';
+        break;
+      case 'DEL':
+        display.innerText=display.innerText.slice(0,-1);
+        break;
+        case '=':
+          if(display.innerText==''){
+            display.innerText="Enter Values";
+          }
+          else{
+          try{
+            display.innerText=eval(display.innerText);
+          }catch{
+            display.innerText='Error!';
+          }
+        }
+          break;
+      default:
+        display.innerText += e.target.innerText;
+        break;
+       
+    }
+  });
+});
